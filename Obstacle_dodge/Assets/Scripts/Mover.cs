55554b3961851956c76ba4f2ea295ba6ba9b8beb
fajlopp <fmoszczynski@gmail.com>
2026-01-11 19:28:58 +0100

    updating mover.cs and creating objecthit.cs

--- Obstacle_dodge/Assets/Scripts/Mover.cs
+++ Obstacle_dodge/Assets/Scripts/Mover.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class Mover : MonoBehaviour
@@ -6,16 +7,28 @@ public class Mover : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        PrintInstructions();
     }
 
     // Update is called once per frame
     void Update()
     {
-     
+        MovePlayer();   
+    }
+
+    void PrintInstructions()
+    {
+        Debug.Log("Welcome to the game");
+        Debug.Log("Don't bump into other objects");
+        Debug.Log("Move by using Arrows or WASD keys");
+    }
+
+    void MovePlayer()
+    {
         float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * movementSpeed;
         float yValue = 0f;
         float zValue = Input.GetAxis("Vertical") * Time.deltaTime * movementSpeed;
         transform.Translate(xValue, yValue, zValue);
     }
+
 }
