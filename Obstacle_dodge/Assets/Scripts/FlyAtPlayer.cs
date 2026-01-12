b9b4cc4dbfaab935f7616c034a9d8a8ff7c063cd
fajlopp <fmoszczynski@gmail.com>
2026-01-12 21:08:36 +0100

    projectiles flying at player after entering trigger

--- Obstacle_dodge/Assets/Scripts/FlyAtPlayer.cs
+++ Obstacle_dodge/Assets/Scripts/FlyAtPlayer.cs
@@ -4,21 +4,32 @@ public class FlyAtPlayer : MonoBehaviour
 {
     [SerializeField] Transform player;
     Vector3 playerPosition;
-    [SerializeField] float projectileSpeed = 0.2f;
+    [SerializeField] float projectileSpeed = 1f;
 
     void Start()
     {
         playerPosition = player.transform.position;
+
     }
 
 
     void Update()
     {
         MoveTowardsPlayer();
+        DestroyWhenReached();
     }
 
     void MoveTowardsPlayer()
     {
         transform.position = Vector3.MoveTowards(transform.position, playerPosition, projectileSpeed * Time.deltaTime);        
+    }
+
+    void DestroyWhenReached()
+    {
+        if (transform.position == playerPosition)
+        {
+            Destroy(gameObject);
+        }
+
     }
 }
